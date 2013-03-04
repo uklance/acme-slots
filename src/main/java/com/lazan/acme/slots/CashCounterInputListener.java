b91a6812a90b8f549dfa20910209399f109585c0
uklance <uklance@gmail.com>
2013-03-04 10:22:28 +0000

    Setup and initial interfaces

--- src/main/java/com/lazan/acme/slots/CashCounterInputListener.java
+++ src/main/java/com/lazan/acme/slots/CashCounterInputListener.java
@@ -1,5 +1,7 @@
 package com.lazan.acme.slots;
 
 public interface CashCounterInputListener {
-
+	public void startBag(String bagId);
+	public void endBag(String bagId);
+	public void bagEntry(String bagId, Denomination denom);
 }
